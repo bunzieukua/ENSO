ec8a85badc4973df37273eab589990e2b27999fe
Le Pham <pnhoangle@gmail.com>
2024-12-02 15:16:46 +0700

    Update 4_oceanic_nino_index.py

--- 4_oceanic_nino_index.py
+++ 4_oceanic_nino_index.py
@@ -6,23 +6,23 @@ We will first calculate the sea surface temperature anomalies of this region,
 then use that anomalies to calculate ONI
 """
 
-"""choose the SST data for the Nino 3.4 region"""
+#choose the SST data for the Nino 3.4 region
 sst_nino34 = sst_anom.sel(lat=slice(-5, 5), lon=slice(190, 240))
 
-"""calculate the mean of sea surface temperature data for this region"""
+#calculate the mean of sea surface temperature data for this region
 nino34_mean = sst_nino34.mean(dim=("lat", "lon"))
 
-"""plotting time series data"""
+#plotting time series data
 fig, ax = plt.subplots(dpi = 300)
 nino34_mean.plot(ax=ax)
 ax.set_xlabel("Time (months)")
 ax.set_ylabel("Nino3.4 Anomaly (°C)")
 ax.axhline(y=0, color="k", linestyle="dashed")
 
-"""calculate ONI"""
+#calculate ONI
 oni = nino34_mean.rolling(time=3, center=True).mean()
 
-"""plot ONI data"""
+#plot ONI data
 fig, ax = plt.subplots(figsize=(12,6), dpi = 300)
 nino34_mean.plot(label = "Nino 3.4", ax=ax)
 oni.plot(color='k', label = "ONI", ax=ax)
@@ -39,7 +39,7 @@ ax.fill_between(
     0.5,
     color="red",
     alpha=0.9,
-) """this generates indicators of El Nino, when ONI is >= +0.5"""
+) #this generates indicators of El Nino, when ONI is >= +0.5
 
 ax.fill_between(
     oni.time.data,
@@ -47,7 +47,7 @@ ax.fill_between(
     -0.5,
     color="blue",
     alpha=0.9,
-) """this generates indicators of La Nina, when ONI is <= -0.5"""
+) #this generates indicators of La Nina, when ONI is <= -0.5
 
 oni.plot(color="black", ax=ax)
 ax.axhline(0, color="black", lw=0.5)
