cd14089712d664d526b8bf7e619d3b96ec22d1b9
Le Pham <pnhoangle@gmail.com>
2024-12-02 13:58:19 +0700

    Update sea_surface_temperature_anomaly.py

--- sea_surface_temperature_anomaly.py
+++ sea_surface_temperature_anomaly.py
@@ -1,28 +1,3 @@
-"""install"""
-!apt-get install libproj-dev proj-data proj-bin --quiet
-!apt-get install libgeos-dev --quiet
-!pip install cython --quiet
-!pip install cartopy --quiet
-!pip install geoviews
-!apt-get -qq install python-cartopy python3-cartopy  --quiet
-!pip uninstall -y shapely  --quiet
-!pip install shapely --no-binary shapely  --quiet
-!pip install boto3 --quiet
-
-"""import"""
-import numpy as np
-import xarray as xr
-import matplotlib.pyplot as plt
-import cartopy
-import cartopy.crs as ccrs
-import os
-import pooch
-import tempfile
-import holoviews
-from geoviews import Dataset as gvDataset
-import geoviews.feature as gf
-from geoviews import Image as gvImage
-from pooch import retrieve
 
 """
 For this code to work, you will have to upload your sea surface temperature data onto GG Drive.
